7cb5ad4aec2cb2aeeac6bb7be953a86770a409ad
Nikkosp <nikkopurnama16@gmail.com>
2025-07-23 12:56:59 +0700

    Add files via upload

---
+++ Project UAS (Laundry Brody)/WinFormLaundry/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WinFormLaundry
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(28, 117, 188);
+            label2.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(75, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Berat (Kg)";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.LightSkyBlue;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(75, 157);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(178, 16);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(28, 117, 188);
+            label3.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(75, 187);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 18);
+            label3.TabIndex = 3;
+            label3.Text = "Layanan";
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.LightSkyBlue;
+            comboBox1.FlatStyle = FlatStyle.Flat;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(75, 207);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(179, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(28, 117, 188);
+            label4.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(75, 300);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 18);
+            label4.TabIndex = 5;
+            label4.Text = "Estimasi Harga";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.LightSkyBlue;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(75, 320);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(178, 16);
+            textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(28, 117, 188);
+            label5.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(76, 353);
+            label5.Name = "label5";
+            label5.Size = new Size(124, 18);
+            label5.TabIndex = 7;
+            label5.Text = "Estimasi Pengambilan";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.LightSkyBlue;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Location = new Point(75, 370);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(178, 16);
+            textBox3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(28, 117, 188);
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(172, 412);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(82, 26);
+            button1.TabIndex = 9;
+            button1.Text = "OKE";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CalendarFont = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.CalendarMonthBackground = SystemColors.ActiveBorder;
+            dateTimePicker1.CalendarTitleBackColor = SystemColors.ControlText;
+            dateTimePicker1.CalendarTitleForeColor = SystemColors.WindowFrame;
+            dateTimePicker1.CalendarTrailingForeColor = SystemColors.ButtonShadow;
+            dateTimePicker1.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Location = new Point(75, 264);
+            dateTimePicker1.Margin = new Padding(3, 2, 3, 2);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(178, 22);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(28, 117, 188);
+            label6.Font = new Font("Tw Cen MT Condensed Extra Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(76, 244);
+            label6.Name = "label6";
+            label6.Size = new Size(48, 18);
+            label6.TabIndex = 11;
+            label6.Text = "Hari Ini";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.ErrorImage = Properties.Resources.l1;
+            pictureBox1.Image = Properties.Resources.l2s_2x;
+            pictureBox1.Location = new Point(75, 11);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(178, 162);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.sa1dd_2x;
+            pictureBox2.Location = new Point(-345, -34);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(893, 562);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DeepSkyBlue;
+            ClientSize = new Size(334, 497);
+            Controls.Add(label6);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private Label label6;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ Project UAS (Laundry Brody)/WinFormLaundry/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace WinFormLaundry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Reguler");
+            comboBox1.Items.Add("Cuci Kering");
+            comboBox1.Items.Add("Cuci VIP");
+            comboBox1.Items.Add("Cuci Antar");
+            comboBox1.SelectedIndex = 0;
+
+            textBox1.TextChanged += new EventHandler(InputChanged);
+            comboBox1.SelectedIndexChanged += new EventHandler(InputChanged);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string berat = textBox1.Text;
+            string layanan = comboBox1.SelectedItem.ToString();
+            string estimasiHarga = textBox2.Text;
+            string estimasiPengambilan = textBox3.Text;
+
+            string pesan = $"--- DETAIL PESANAN ANDA ---\n" +
+                           $"Berat Cucian     : {berat} kg\n" +
+                           $"Layanan          : {layanan}\n" +
+                           $"Estimasi Harga   : {estimasiHarga}\n" +
+                           $"Estimasi Selesai : {estimasiPengambilan}";
+
+            MessageBox.Show(pesan, "Konfirmasi Pesanan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void InputChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text)) return;
+
+                double berat = double.Parse(textBox1.Text);
+                double unit3kg = berat / 1.0;
+
+                double hargaDasar = unit3kg * 4000;
+
+                string layanan = comboBox1.SelectedItem.ToString();
+                double tambahan = 0;
+                int estimasiHari = 3;
+
+                switch (layanan)
+                {
+                    case "Reguler":
+                        tambahan = unit3kg * 1000;
+                        estimasiHari = 3;
+                        break;
+                    case "Cuci Kering":
+                        tambahan = unit3kg * 1500;
+                        estimasiHari = 3;
+                        break;
+                    case "Cuci VIP":
+                        tambahan = unit3kg * 3000;
+                        estimasiHari = 1;
+                        break;
+                    case "Cuci Antar":
+                        tambahan = unit3kg * 2000;
+                        estimasiHari = 2;
+                        break;
+                }
+                double totalHarga = hargaDasar + tambahan;
+                double estimasiHarga = unit3kg * 2;
+                DateTime mulai = dateTimePicker1.Value.Date;
+                DateTime selesai = mulai.AddDays(estimasiHari);
+
+                textBox2.Text = $"Rp {totalHarga}";
+                textBox3.Text = selesai.ToString("dd/MM/yyy");
+            }
+            catch
+            {
+                textBox2.Text = "-";
+                textBox3.Text = "-";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
